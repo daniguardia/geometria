13933ffd82c1bec98f9c4354da5e9490681bdfcf
Daniel Guardia <daniguardiamail@gmail.com>
2020-01-19 12:35:15 +0100

    versión refactorizada

--- geometria/src/FiguraGeometrica_DGM.java
+++ geometria/src/FiguraGeometrica_DGM.java
@@ -1,9 +1,9 @@
 
-public abstract class FiguraGeometrica {
+public abstract class FiguraGeometrica_DGM {
 
 	protected String tipoFigura;
 
-	public FiguraGeometrica(String tipoFigura) {
+	public FiguraGeometrica_DGM(String tipoFigura) {
 		super();
 		this.tipoFigura = tipoFigura;
 	}
--- geometria/src/Rectangulo_DGM.java
+++ geometria/src/Rectangulo_DGM.java
@@ -1,9 +1,9 @@
 
-public class Rectangulo extends FiguraGeometrica {
+public class Rectangulo_DGM extends FiguraGeometrica_DGM {
 	private double l1;
 	private double l2;
 	
-	public Rectangulo(String tipoFigura, double lG, double lP) {
+	public Rectangulo_DGM(String tipoFigura, double lG, double lP) {
 		super(tipoFigura);
 		l1 = lG;
 		l2 = lP;
