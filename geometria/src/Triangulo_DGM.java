0f5d835e7d8cb2446a575edfa84fd55496ca818a
Daniel Guardia <daniguardiamail@gmail.com>
2020-01-19 12:45:42 +0100

    nuevo método

--- geometria/src/Triangulo_DGM.java
+++ geometria/src/Triangulo_DGM.java
@@ -22,8 +22,5 @@ public class Triangulo_DGM extends FiguraGeometrica_DGM {
 		sp = this.semiPerimetro();
 		return Math.sqrt(sp * (sp-lado1) * (sp-lado2) * (sp-lado3));
 	}
-	
-	public double semiPerimetro() {
-		return this.perimetro() / 2;
-	}
+
 }
